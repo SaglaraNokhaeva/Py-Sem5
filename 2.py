2af2a090d6769d313715c646d5f7c7a80f1e9f7c
Saglara <nohaevass@yandex.ru>
2022-12-08 20:18:52 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,11 +1,25 @@
 # нод
 
-a = int(input('Введите а: '))
-b = int(input('Введите в: '))
-while a != b:
-    if a > b:
-        a = a - b
-    else:
-        b = b - a
+# a = int(input('Введите а: '))
+# b = int(input('Введите в: '))
+# while a != b:
+#     if a > b:
+#         a = a - b
+#     else:
+#         b = b - a
 
-print(a)
+# print(a)
+
+
+
+import random
+a = random.randint(33, 99)
+b = random.randint(33, 99)
+c = [a, b]
+print(c)
+while max(a, b) % min(a, b) != 0:
+    if a > b:
+        a = a % b
+    elif a < b:
+        b = b % a
+print(min(a, b))
